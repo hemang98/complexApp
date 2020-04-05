1dd07ec6d4fbdce3738ed32dc03ef653d31628be
Vora <hemangvora2@gmail.com>
2020-04-05 15:59:55 +0530

    making iit better

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+
+let port = process.env.PORT
+if(port == null || port =="")
+{
+  port = 3000
+}
 app.use(express.static('public'))
 app.set('views','views')
 app.set('view engine','ejs')
 app.get('/',function(req,res){
 res.render('home-guest')
 })
-app.listen(3000)
+app.listen(port)
